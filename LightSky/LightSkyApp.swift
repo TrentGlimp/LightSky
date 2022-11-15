a924078d3436d2af9d640f4036e9fc177d89334d
Trent Glimp <chitr8or@gmail.com>
2022-11-15 11:05:28 -0600

    Empty File

---
+++ LightSky/LightSkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Light_SkyApp.swift
+//  Light Sky
+//
+//  Created by Trent Glimp on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Light_SkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
